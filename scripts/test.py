6c8cac0a5c01b6cbeb6472016d4b3442a48402ef
npotts <nicholas.potts@gmail.com>
2015-01-21 23:29:55 -0700

    Added in temp reading

--- scripts/test.py
+++ scripts/test.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
   g = gsensors(cfg)
   while(True):
     v = g.readValue(0);
-    sys.stdout.write("\r%04d (%%%3.2f)" % (v, 100.0 * v / 4096.0))
+    t = g.readValue(1);
+    sys.stdout.write("\r%04d (%%%3.2f) - %2.4fC" % (v, 100.0 * v / 4096.0, t))
     sys.stdout.flush()
     time.sleep(.1);
