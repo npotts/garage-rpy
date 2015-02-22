68debc70e54514cb1f008be2fd938a3530ff6158
me <noemail@nowhere.com>
2015-02-21 23:50:08 -0700

    Added CherryPy based web front end.  Really simple, first AJAX webpage I have ever done, but useful enough!  TODO is to tie it to the real hardware (not as complicated)

---
+++ cherrypy-garagepy.py
@@ -0,0 +1,69 @@
+import os, os.path
+import random
+import string
+from random import randint
+
+import cherrypy
+
+class StringGenerator(object):
+   @cherrypy.expose
+   def index(self):
+       return open('www/index.html')
+
+class GaragePyWebService(object):
+    exposed = True
+
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        return cherrypy.session['mystring']
+
+    def POST(self, smashit=None, length=None, pos=None):
+        if smashit:
+        #     data_value='{"units": ["%%", "C"], "counts": [1081, 14], "values": [%d, 34.32199599497977]}'
+        # #get some values from the unit.   We will refresh every 10 seconds or so
+        # rtn = html_core__
+        # progress_bars = ""
+        # d = json.loads(data_value % randint(0, 100))
+        # print(d)
+        # for i in range(len(d["values"])):
+        #     progress_bars += "<br />\n" + progress_bar__ % (int(d["values"][i]),  "Test ", d["units"][i])
+        #     print(d["values"][i])
+        # return rtn % progress_bars;
+            # print("\n\nSmashit pressed")
+            cherrypy.session["doneit"] = "Yeah baby"
+            return "Garage Button Triggered."
+        if length:
+            # print("\n\nGive it now pressed")
+            some_string = ''.join(random.sample(string.hexdigits, int(length)))
+            cherrypy.session['mystring'] = "ADSASDA " + some_string
+            return some_string
+        if pos:
+            d = randint(0, 100)
+            return '{"pos":%d, "spos":"%s"}' % (d,  "closed" if d > 90 else "open")
+        return "ALL SORTS OF CRAP"
+
+    def PUT(self, another_string):
+        cherrypy.session['mystring'] = another_string
+
+    def DELETE(self):
+        cherrypy.session.pop('mystring', None)
+
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/generator': {
+            'request.dispatch': cherrypy.dispatch.MethodDispatcher(),
+            'tools.response_headers.on': True,
+            'tools.response_headers.headers': [('Content-Type', 'text/plain')],
+        },
+        '/www': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './www'
+        }
+    }
+    webapp = StringGenerator()
+    webapp.generator = GaragePyWebService()
+    cherrypy.quickstart(webapp, '/', conf)
